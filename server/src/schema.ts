4ad4c01ca011f2b027011065846d193e925b88c3
danieldupreez <daniel@trooptravel.com>
2023-12-19 16:54:47 +0400

    Add getTrack method to TrackAPI

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -40,9 +40,10 @@ export const typeDefs = gql`
 
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
-  id: ID!
-  "The Module's title"
-  title: String!
-  "The Module's length in minutes"
-  length: Int
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
 `;
