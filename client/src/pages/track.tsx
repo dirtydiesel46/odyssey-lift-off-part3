fd82a859c2d3a0d2ef95fc8d931765ff97eb715b
danieldupreez <daniel@trooptravel.com>
2023-12-19 17:12:41 +0400

    Introduce empty Track page

---
+++ client/src/pages/track.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { gql } from "../__generated__";
+import { useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import { useParams } from "react-router-dom";
+
+const Track = () => {
+  const { trackId = "" } = useParams();
+  return <Layout></Layout>;
+};
+
+export default Track;
